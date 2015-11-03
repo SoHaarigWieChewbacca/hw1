d4ac5cd4b1c83658ae142f77406331ad8ce8f72f
cadie101 <cadie101@linux-mb65.site>
2015-11-03 21:53:35 +0100

    hw1

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+
+	int N;	
+	cout << "N" << endl;
+	cin >> N;
+	
+	int f0 = 0;
+	int f1 = 1;
+	int f;
+	
+	for(int i=2; i<=N; i++) {
+		f = f1 + f0;
+		f0 = f1;
+		f1 = f;
+	}
+
+	cout << "f = " << f << endl;
+
+	return 0;
+}
